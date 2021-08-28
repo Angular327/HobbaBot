9bb05c1fa005f4f12652c1a2a843bd21d4406f61
milohk <30414356+Angular327@users.noreply.github.com>
2021-08-27 23:23:30 -0400

    Complete Checkin Bot

--- app.js
+++ app.js
@@ -3,29 +3,26 @@ const Discord = require("discord.js");
 const intents = new Discord.Intents(32767);
 const client = new Discord.Client({ intents });
 const config = require("./config.json");
+var cron = require('node-cron');
 
-//Id for Roles to set
-const done = "881001258322903131";
-const notDone = "881001194586263555";
-
-//On Bot Run
-client.on('ready', () => {
-  //Says the bot logs in
-  console.log(`Logged in as ${client.user.tag}!`);
-
+cron.schedule('00 12 * * Saturday', () => {
   //get list of all users
   const list = client.guilds.cache.get(config.guildId); 
 
   //set users to not done role. They can become done by checking in
   list.members.cache.forEach(member => {
     if(!member.user.bot) {
-      member.roles.add(notDone)
-      member.roles.remove(done);
+      member.roles.add(config.notDone)
+      member.roles.remove(config.done);
     }
   }); 
+});
 
-  //Let's the console know setup compelete
-  console.log("Setup Complete")
+//On Bot Run
+client.on('ready', () => {
+  //Says the bot logs in
+  console.log(`Logged in as ${client.user.tag}!`);
+
 });
 
 //on message in chat this will be read
@@ -33,10 +30,10 @@ client.on('messageCreate', message => {
     //if the message is !checkin
     if(message.content == "!checkin") {
       //If the user doesn't already have the done role
-      if(!message.member.roles.cache.has(done)) {
+      if(!message.member.roles.cache.has(config.done)) {
         //Give the user the done role and remove the not done role
-        message.member.roles.add(done);
-        message.member.roles.remove(notDone);
+        message.member.roles.add(config.done);
+        message.member.roles.remove(config.notDone);
 
         //Tell the user they have checked in
         message.reply("You have checked in");
